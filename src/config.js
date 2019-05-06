de03e92dd30729181535d3754514d178593c8a62
José Abarca Saavedra <jose@smart.rancagua.cl>
2019-05-06 12:54:26 -0400

    public files

--- src/config.js
+++ src/config.js
@@ -26,7 +26,7 @@ const config = {
     port: process.env.PORT || 9000,
     ip: process.env.IP || '0.0.0.0',
     apiRoot: process.env.API_ROOT || '',
-    masterKey: requireProcessEnv('NODE_ENV'),
+    masterKey: requireProcessEnv('MASTER_KEY'),
     domain: 857,
     ws: {
       service: {
